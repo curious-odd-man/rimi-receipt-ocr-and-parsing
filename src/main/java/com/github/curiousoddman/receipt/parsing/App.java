e396e5a1db6f41c3f044f22a68f26738622a96d0
curious_odd_man <vlad.varslavans@gmail.com>
2023-10-14 21:59:00 +0300

    adding receipts parsing

--- src/main/java/com/github/curiousoddman/receipt/parsing/App.java
+++ src/main/java/com/github/curiousoddman/receipt/parsing/App.java
@@ -2,7 +2,6 @@ package com.github.curiousoddman.receipt.parsing;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
