af9dc31e9ba5e5045a51f3438d983341a16db689
curious_odd_man <vlad.varslavans@gmail.com>
2023-10-21 12:45:49 +0300

    Item numbers prices should be rounded to 2 digits

--- src/main/java/com/github/curiousoddman/receipt/parsing/validation/ItemNumbersValidator.java
+++ src/main/java/com/github/curiousoddman/receipt/parsing/validation/ItemNumbersValidator.java
@@ -5,6 +5,7 @@ import com.github.curiousoddman.receipt.parsing.model.ReceiptItem;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class ItemNumbersValidator implements ReceiptValidator {
         List<Object> errors = new ArrayList<>();
         for (ReceiptItem item : receipt.getItems()) {
             BigDecimal fullPrice = item.getCount().multiply(item.getPricePerUnit());
-            BigDecimal discountedPrice = fullPrice.subtract(item.getDiscount());
+            BigDecimal discountedPrice = fullPrice.subtract(item.getDiscount()).setScale(2, RoundingMode.HALF_UP);
             if (discountedPrice.compareTo(item.getFinalCost()) != 0) {
                 errors.add(
                         String.format("Items numbers are inconsistent: %s", item)
