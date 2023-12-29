3abe19e78acd9621e61f42276dc89f0ba5d518e4
curious_odd_man <vlad.varslavans@gmail.com>
2023-12-29 11:37:28 +0200

    These steps do not add any value - never helped

---
+++ src/main/java/com/github/curiousoddman/receipt/alt/main/PreprocessImage.java
@@ -0,0 +1,15 @@
+package com.github.curiousoddman.receipt.alt.main;
+
+import com.github.curiousoddman.receipt.parsing.opencv.OpenCvUtils;
+import nu.pattern.OpenCV;
+
+import java.nio.file.Path;
+
+public class PreprocessImage {
+    public static void main(String[] args) {
+        OpenCV.loadLocally();
+        var source = Path.of("D:\\Programming\\git\\caches\\cache\\2-214693\\2-214693.pdf.tiff");
+        var target = Path.of("D:\\Programming\\git\\caches\\cache\\2-214693\\2-214693.pdf.preprocessed_test.tiff");
+        OpenCvUtils.doImagePreprocessing(source, target);
+    }
+}
--- src/main/java/com/github/curiousoddman/receipt/parsing/parsing/receipt/rimi/ReceiptNumberExtractionChain.java
+++ src/main/java/com/github/curiousoddman/receipt/parsing/parsing/receipt/rimi/ReceiptNumberExtractionChain.java
@@ -95,29 +95,6 @@ public class ReceiptNumberExtractionChain {
 
         triedValues.add("re-ocr word: " + value);
 
-        return reOcrWordLocationInOriginalTiff(originalWord, wordIndexInLine, type);
-    }
-
-    private NumberOcrResult reOcrWordLocationInOriginalTiff(TsvWord originalWord, int wordIndexInLine, String type) {
-        String value = null;
-        Rectangle originalWordRectangle = originalWord.getWordRect();
-        try {
-            OcrConfig ocrConfig = OcrConfig
-                    .builder(context.getOriginFile().convertedTiff())
-                    .ocrDigitsOnly(true)
-                    .ocrArea(originalWordRectangle)
-                    .build();
-            value = tesseract.doOCR(ocrConfig);
-            if (isFormatValid(expectedFormat, value)) {
-                tsvWordConsumer.accept(originalWord);
-                return NumberOcrResult.of(toMyBigDecimal(value), originalWordRectangle);
-            }
-        } catch (TesseractException ex) {
-            log.error(ex.getMessage(), ex);
-        }
-
-        triedValues.add("re-ocr original file: " + value);
-
         return reOcrWordLine(originalWord, wordIndexInLine, type);
     }
 
@@ -151,37 +128,7 @@ public class ReceiptNumberExtractionChain {
             log.error(ex.getMessage(), ex);
         }
 
-        return reOcrWordLineInOriginalTiff(originalWord, wordIndexInLine, type);
-    }
-
-    private NumberOcrResult reOcrWordLineInOriginalTiff(TsvWord originalWord, int wordIndexInLine, String type) {
-        TsvLine line = null;
-        String text;
-        try {
-            OcrConfig ocrConfig = OcrConfig
-                    .builder(context.getOriginFile().convertedTiff())
-                    .ocrArea(originalWord.getParentLine().getRectangle())
-                    .ocrToTsv(true)
-                    .build();
-            // Finally try to re-ocr whole line and get the word by the same word num.
-            String tsvText = tesseract.doOCR(ocrConfig);
-            TsvDocument tsvDocument = tsv2Struct.parseTsv(tsvText);
-            line = tsvDocument.getLines().get(0);
-            Optional<TsvWord> wordByWordNum = line.getWordByIndex(wordIndexInLine);
-            if (wordByWordNum.isPresent()) {
-                TsvWord tsvWord = wordByWordNum.get();
-                text = tsvWord.getText();
-                if (isFormatValid(expectedFormat, text)) {
-                    tsvWordConsumer.accept(tsvWord);
-                    allNumberCollector.add(context.getOriginFile().preprocessedTiff(), type, tsvWord);
-                    return NumberOcrResult.of(toMyBigDecimal(text), tsvWord.getWordRect());
-                }
-            }
-            triedValues.add("re-ocr original file line: idx=" + originalWord.getWordNum() + "; line=" + line.getText());
-        } catch (Exception ex) {
-            triedValues.add(ex.getMessage());
-            log.error(ex.getMessage(), ex);
-        }
+        //return reOcrWordLineInOriginalTiff(originalWord, wordIndexInLine, type);
         return reOcrByCorrection(originalWord);
     }
 
